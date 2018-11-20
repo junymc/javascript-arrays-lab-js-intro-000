baedad13d67a49c841de3d3b8b9ebbc237b443a9
Junyoung McArdle <juny.mcardle@gmail.com>
2018-11-20 19:32:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function destructivelyAppendKitten(name) {
   kittens.push(name)
   return kittens
 }// Add your functions and code here
+
+function destructivelyPrependKittens(name) {
+  kittens.unshift(name)
+  return kittens
+}
